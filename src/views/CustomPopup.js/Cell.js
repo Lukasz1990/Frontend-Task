9188999187ea3fba4e5ddbfc16ae31fd824657b5
Łukasz Fijałkowski <lukasz.fijalkowski1337@gmail.com>
2020-03-25 21:37:20 +0100

    innitial Commit

---
+++ src/views/CustomPopup.js/Cell.js
@@ -0,0 +1,160 @@
+import styled, { css } from 'styled-components';
+import React, { useState } from 'react';
+import { useSelector,useDispatch } from 'react-redux'
+import { removeDataSubCell } from '../redux/removeDataSubCell'
+import { addDataSubCell } from '../redux/addDataSubCell'
+import Popup from './Popup'
+
+const S = {
+
+    Cell: styled.div`
+    display:flex;
+    justify-content:space-between;
+    font-size:20px;
+    font-weight:700;
+    color:#4d6285;
+    border:1px solid #eee;
+    padding:10px;
+    padding-left:30px;
+    margin-left:10px;
+    border-radius:6px;
+    position:relative;
+    width:400px;
+    margin:15px;
+    ${({ smaller }) => smaller && css`
+    border:1px solid #eee;
+    width:300px;
+    font-size:14px;
+    margin-left:60px;
+    &:before {
+      content:'';
+      position: absolute;
+
+    ,
+  `}
+    ${({ extended }) => extended && css`
+    border:1px solid #eee;
+    width:300px;
+    font-size:14px;
+    &:before {
+      content:'';
+      position: absolute;
+
+    ,
+  `}
+    &:before {
+      content:'';
+      position: absolute;
+      width:8px;
+      height:100%;
+      background:#8f76f5;
+      border-top-left-radius:6px;
+      border-bottom-left-radius:6px;
+      left:0;
+      margin-top:-10px;
+    }
+    `,
+      
+      CellExtended:styled.div`
+      ${({ extended }) => extended && css`
+      border:1px solid #eee;
+      width:400px;
+      height:auto;
+      border-radius:6px;
+      margin-left:15px;
+      position:relative;
+      padding:10px;
+    
+    &:before {
+      content:'';
+      position: absolute;
+      width:8px;
+      height:100%;
+      background:#8f76f5;
+      border-top-left-radius:6px;
+      border-bottom-left-radius:6px;
+      left:0;
+      margin-top:-10px;
+
+    ,
+      
+    `}
+      `,
+      IconContainer: styled.span`
+      display: flex;
+      margin-right:10px;
+      ${({ extended }) => extended && css`
+      margin-right:-60px;
+      
+      `}
+      ${({ smaller }) => smaller && css`
+      margin-right:-5px;
+      
+      `}
+      ${({ add }) => add && css`
+      margin-left:30px;
+      
+      `}
+      `,
+      RemmoveIcon: styled.img`
+      width: 24px;
+      height: 24px;
+      cursor: pointer;
+      `,
+      AddIcon: styled.img`
+      margin-right: 10px;
+      width: 34px;
+      height: 34px;
+      cursor: pointer;
+    `,
+}
+
+const Cell = ({ children,extended,smaller }) => {
+
+  const extendedCells = useSelector(state=>state.dataReducer.extendedCells)
+  const dispatch = useDispatch()
+  
+  const [value, setValue] = useState('');
+  const [isOpen, toggleModal] = useState(false)
+
+  const onChangeHandler = (e) => {
+    setValue(e.target.value)
+  }
+
+  const addCellHandler = () => {
+    if (value !== '') {
+      dispatch(addDataSubCell(
+        {
+          value,
+          'id':Math.random()
+        }
+      ))
+      toggleModal(false)        
+    }
+  }
+  
+    
+    return (
+    <S.CellExtended extended={extended}>
+              <Popup  isOpen={isOpen} cancel={() => toggleModal(false)} onChangeHandler={onChangeHandler} addCellHandler={addCellHandler}/>
+      <S.Cell extended={extended}>
+        {children}
+      </S.Cell>
+      {extended &&  extendedCells.map(({ id, value })=>(
+                <>
+                <S.Cell key={id} smaller>{value}
+                  <S.IconContainer smaller>
+                    <S.RemmoveIcon  src="../static/images/minus.png"  onClick={() => dispatch(removeDataSubCell(id))}/>
+                  </S.IconContainer>
+                </S.Cell>
+                  </>
+           ))}
+      {extended && 
+                <S.IconContainer add>
+                      <S.AddIcon  src="../static/images/more.png"  onClick={() => toggleModal(true)}/>
+                  </S.IconContainer>}
+    </S.CellExtended>
+    )
+  }
+
+export default Cell
---
+++ src/views/CustomPopup.js/index.js
@@ -0,0 +1,152 @@
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import Cell from '../components/Cell'
+import Popup from './Popup'
+import { useSelector,useDispatch } from 'react-redux'
+import { removeData } from '../redux/removeData'
+import { addData } from '../redux/addData'
+import { makeStyles } from '@material-ui/styles';
+import Page from 'src/components/Page';
+import { makeStyles } from '@material-ui/styles';
+import {
+  Container,
+  Divider,
+  Typography,
+  Grid,
+  colors,
+  Button
+} from '@material-ui/core';
+
+const S = {
+
+    Button: styled.button`
+      background-color: rgba(251, 253, 254, 1);
+      box-sizing: border-box;
+      border-width: 1px;
+      border-style: solid;
+      border-color: rgba(204, 204, 204, 1);
+      box-shadow: none;
+      border-radius: 40px;
+      font-size: 14px;
+      color: rgba(57, 67, 68, 0.8);
+      width: 100px;  
+      height: 40px;
+      font-weight: bold;
+      font-style: normal;
+      margin-top:10px;
+      cursor:pointer;
+      margin:5px;
+      &:hover {
+        box-shadow: 0px 2px 4px rgba(57, 67, 68, 0.349019607843137);
+        background-color:#ddd;
+        color:#fff;
+      }
+    `,
+    IconContainer: styled.span`
+    display: flex;
+    margin-right:10px;
+    ${({ extended }) => extended && css`
+    margin-right:-60px;
+    
+  `}
+    ${({ add }) => add && css`
+    margin-left:-60px;
+    margin-top:30px;
+  `}
+  `,
+    RemmoveIcon: styled.img`
+    margin-right: 10px;
+    width: 24px;
+    height: 24px;
+    cursor: pointer;
+  `,
+    AddIcon: styled.img`
+    margin-right: 10px;
+    width: 54px;
+    height: 54px;
+    cursor: pointer;
+  `,
+   People: styled.div`
+   width:200px;
+   background:#8f76f5;
+   height:50px;
+   padding:20px;
+   display:flex;
+   border-radius:6px;
+   font-size:20px;
+   font-weight:600;
+   justify-content:center;
+   align-items:center;
+   color:#FFF;
+   margin-left:-140px;
+   margin-bottom:20px;
+   `,
+}
+const useStyles = makeStyles((theme) => ({
+  root: {
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3)
+  },
+  divider: {
+    backgroundColor: colors.grey[300],
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3)
+  }
+}));
+
+
+function CustomPopup  ({}) {
+  
+
+
+  // const data = useSelector(state=>state.dataReducer.data)
+  // const dispatch = useDispatch()
+
+  // const [value, setValue] = useState('');
+  // const [isOpen, toggleModal] = useState(false)
+
+  // const onChangeHandler = (e) => {
+  //   setValue(e.target.value)
+  // }
+
+  // const addCellHandler = () => {
+  //   if (value !== '') {
+  //     dispatch(addData(
+  //       {
+  //         value,
+  //         'id':Math.random()
+  //       }
+  //     ))
+  //     toggleModal(false)        
+  //   }
+  // }
+  
+
+  const classes = useStyles();
+
+  return (
+    <Page
+      className={classes.root}
+      title="CustomPopup"
+    >
+      <Container maxWidth="md">
+       <S.People>People</S.People>
+        {/* <Popup  isOpen={isOpen} cancel={() => toggleModal(false)} onChangeHandler={onChangeHandler} addCellHandler={addCellHandler}/>
+        {data.map(({ id, value })=> (
+          <Cell key={id} extended={value==='Ethncity'} >{value} 
+            <S.IconContainer extended={value==='Ethncity'}>
+              <S.RemmoveIcon  src="../static/images/minus.png"  onClick={() => dispatch(removeData(id))}/>
+            </S.IconContainer>
+          </Cell>
+        ))}
+            <S.IconContainer add>
+              <S.AddIcon  src="../static/images/more.png"  onClick={() => toggleModal(true)}/>
+            </S.IconContainer> */}
+
+     </Container>
+     </Page>
+    )
+
+}
+
+export default CustomPopup
